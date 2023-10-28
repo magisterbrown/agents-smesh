910ebe260dc1309f7e6a85cc055f7c36106216ea
Artur <magisterbrownie@gmail.com>
2023-10-28 10:27:46 +0200

    playing a game from go

--- games/play.py
+++ games/play.py
@@ -1,23 +1,27 @@
 from pettingzoo.classic import rps_v2
 import json
+import time
 
-env = rps_v2.env()
+env = rps_v2.env(max_cycles=2)
 env.reset(seed=42)
 acc_rewards = env.rewards.copy()
 
 itt = env.agent_iter()
 for agent in env.agent_iter():
     observation, reward, termination, truncation, info = env.last()
-    print(json.dumps({'agent':agent, 'observation':observation.tolist()}))
     if termination or truncation:
         action = None
     else:
         # this is where you would insert your policy
-        action = env.action_space(agent).sample()
+        action = input(json.dumps({"type": "move", "agent": agent, "observation": observation.tolist()}))
+        action = int(action)
 
     env.step(action)
+    time.sleep(3)
     for agent, reward in env.rewards.items():
         acc_rewards[agent]+=reward
 env.close()
-print(json.dumps(acc_rewards))
-print("aa")
+winner = max(acc_rewards, key=acc_rewards.get)
+if all(value == 0 for value in acc_rewards.values()):
+    winner = None
+print(json.dumps({"type": "result", "winner": winner}))
--- gbild.go
+++ gbild.go
@@ -7,11 +7,12 @@ import (
 	"github.com/docker/docker/pkg/archive"
     "context"
     "fmt"
+    "encoding/json"
 )
 
 func main() {
 	cli, err := client.NewClientWithOpts(client.FromEnv)
-    file, err := archive.TarWithOptions("games/", &archive.TarOptions{IncludeFiles: []string{"Dockerfile", "test.py", "requirements.txt"}})
+    file, err := archive.TarWithOptions("games/", &archive.TarOptions{IncludeFiles: []string{"Dockerfile", "test.py","play.py", "requirements.txt"}})
     var tag = "game:latest"
     options := types.ImageBuildOptions{
         Tags: []string{tag},
@@ -19,24 +20,50 @@ func main() {
         Dockerfile: "Dockerfile",           
     }
     resp, err := cli.ImageBuild(context.Background(), file, options)
-    //container, err := 
-    conf := container.Config{Image: tag, AttachStdin: true, AttachStdout: true, AttachStderr: true, Tty: true, OpenStdin: true, StdinOnce: true}
-    container, err := cli.ContainerCreate(context.Background(), &conf, nil, nil, nil, "coolgame")
-    fmt.Println(container.ID)
-    hijack, err := cli.ContainerAttach(context.Background(), container.ID, types.ContainerAttachOptions{Stream:true, Stdout:true, Stdin:true, Stderr:true})
-    cli.ContainerStart(context.Background(), container.ID, types.ContainerStartOptions{})
-    //execResp, err := 
     if err != nil {
 	    fmt.Println("Error:", err)
 	    return
 	}
-    //io.Copy(os.Stdout, hijack.Reader)
+    //Starter 
+    conf := container.Config{Image: tag, AttachStdin: true, AttachStdout: true, AttachStderr: true, Tty: true, OpenStdin: true, StdinOnce: true}
+    container, err := cli.ContainerCreate(context.Background(), &conf, nil, nil, nil, "coolgame")
+    hijack, err := cli.ContainerAttach(context.Background(), container.ID, types.ContainerAttachOptions{Stream:true, Stdout:true, Stdin:true, Stderr:true})
+    cli.ContainerStart(context.Background(), container.ID, types.ContainerStartOptions{})
+    if err != nil {
+	    fmt.Println("Error:", err)
+	    return
+	}
+    //Play
+    //TODO: react to random commands with skip
     buf := make([]byte, 1024)
-    sz, err := hijack.Conn.Read(buf)
-    fmt.Printf(string(buf[:sz]))
-    hijack.Conn.Write([]byte("Message received.\n"))
-    sz, err = hijack.Conn.Read(buf)
-    fmt.Printf(string(buf[:sz]))
+    var message map[string]interface{}
+    var sz int
+    movesLoop:
+    for i:=0; i<6; i++ {
+        sz, err = hijack.Conn.Read(buf)
+        json.Unmarshal(buf[:sz], &message)
+        intype, _ := message["type"].(string)
+        switch intype {
+            case "move": {
+                fmt.Printf(intype)
+                hijack.Conn.Write([]byte("1\n"))
+                hijack.Conn.Read(buf)
+                for key, value := range message{
+                    fmt.Printf("%s: %v\n", key, value)
+                }
+            }
+            case "result": {
+                    fmt.Printf("WInner")
+                    fmt.Printf(string(buf[:sz]))
+                    break movesLoop
+                }
+            default: {
+                    fmt.Printf("Errror out")
+                    break movesLoop
+            }
+        }
+    }
+
     defer hijack.Close()
     //cli.ContainerExecStart(context.Background(), execResp.ID, types.ExecStartCheck{})
     //resp, err := cli.ContainerExecCreate(context.Background(), file, options)
@@ -45,5 +72,9 @@ func main() {
 	//    fmt.Println("Error:", err2)
 	//    return
 	//}
+    if err != nil {
+	    fmt.Println("Error:", err)
+	    return
+	}
     defer resp.Body.Close()
 }
